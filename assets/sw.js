cfd3024ef7413b6d7abb36008262e3bc68a26d05
OSCARWB <oscarwb2000@gmail.com>
2022-11-01 21:55:21 +1100

    changed things to be not like template

--- assets/sw.js
+++ assets/sw.js
@@ -2,8 +2,8 @@ var cacheName = 'egui-template-pwa';
 var filesToCache = [
   './',
   './index.html',
-  './eframe_template.js',
-  './eframe_template_bg.wasm',
+  './TAS_DIAS.js',
+  './TAS_DIAS.wasm',
 ];
 
 /* Start the service worker and cache all of the app's content */
